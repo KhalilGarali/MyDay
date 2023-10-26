fefef2ff70ffaafb0e52b0e7acac62e3b9ef497b
Khalil Garaali <garalikhalil@gmail.com>
2023-10-26 00:59:36 -0400

    Sidebar

---
+++ src/Pages/Home.js
@@ -0,0 +1,32 @@
+import MotionHoc from "./MotionHoc";
+
+const HomeComponent = () => {
+  return (
+    <>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    </>
+  );
+};
+
+const Home = MotionHoc(HomeComponent);
+
+export default Home;
